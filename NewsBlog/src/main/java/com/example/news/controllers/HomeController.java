8b4139a2bc85d8119632f81306f9356f6ceb11d7
VladNazarov <vladgitar99@gmail.com>
2020-06-05 13:21:56 +0400

    Добавил пагинацию страниц

--- NewsBlog/src/main/java/com/example/news/controllers/HomeController.java
+++ NewsBlog/src/main/java/com/example/news/controllers/HomeController.java
@@ -8,7 +8,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
@@ -37,6 +36,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class HomeController {
+	final int AMOUNT_OF_NEWS = 5;
 
 	@Autowired
 	UsersStorage usersStorage;
@@ -45,7 +45,8 @@ public class HomeController {
 	BlogsStorage blogStorage;
 
 	@GetMapping({ "", "/", "home" })
-	public String homePage(Model model, @RequestParam(name = "slug", required = false) String slug)
+	public String homePage(Model model, @RequestParam(name = "id", required = false, defaultValue = "1") Integer reqId,
+			@RequestParam(name = "slug", required = false) String slug)
 			throws URISyntaxException, IOException, InterruptedException {
 
 		var httpClient = HttpClient.newHttpClient();
@@ -59,28 +60,50 @@ public class HomeController {
 		objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
 
 		var lenta = objectMapper.readValue(response.body(), LentaHeadlines.class);
-		
+
 		List<LentaNews> filteredNews = new ArrayList<LentaNews>();
-		
+
 		if (slug == null) {
-			//model.addAttribute("news", lenta.getHeadlines());
+			// model.addAttribute("news", lenta.getHeadlines());
 			filteredNews = lenta.getHeadlines();
 		} else {
 			for (var item : lenta.getHeadlines()) {
 				if (item.getRubric().getSlug().equals(slug)) {
 					filteredNews.add(item);
+
 				}
 			}
 		}
-		
 
 		HashSet<LentaNewsRubric> rubrics = new HashSet<LentaNewsRubric>();
 		for (var item : lenta.getHeadlines()) {
 			rubrics.add(item.getRubric());
 		}
 
+		List<Integer> pagNum = new ArrayList<>();
+
+		for (int i = 1; i < filteredNews.size() / AMOUNT_OF_NEWS; i++) {
+			pagNum.add(i);
+		}
+
+		if (filteredNews.size() >= 5) {
+			List<LentaNews> result = new ArrayList<LentaNews>();
+
+			int start = (reqId - 1) * AMOUNT_OF_NEWS;
+			int end = AMOUNT_OF_NEWS * reqId;
+
+			for (int i = start; i < end; i++) {
+				result.add(filteredNews.get(i));
+
+			}
+			model.addAttribute("news", result);
+
+		} else {
+			model.addAttribute("news", filteredNews);
+		}
+
+		model.addAttribute("nums", pagNum);
 		model.addAttribute("rubrics", rubrics);
-		model.addAttribute("news", filteredNews);
 
 		return "views/home";
 	}
