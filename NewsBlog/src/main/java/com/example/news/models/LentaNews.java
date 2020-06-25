7daf9b17576fbe3a04aea3d84656a89e83b1aa96
VladNazarov <vladgitar99@gmail.com>
2020-06-25 19:10:26 +0400

    Удалил дублёр класса

--- NewsBlog/src/main/java/com/example/news/models/LentaNews.java
+++ NewsBlog/src/main/java/com/example/news/models/LentaNews.java
@@ -1,7 +1,5 @@
 package com.example.news.models;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -23,7 +21,6 @@ public class LentaNews extends NewsItem {
 	private LentaNewsInfo info;
 	private LentaNewsLink links;
 	private LentaNewsRubric rubric;
-	private List<LentaNewsTag> tags;
 
 	@JsonProperty("title_image")
 	private LentaNewsImage titleImage;
