de1417679707b4beff919a5dd893e8f5baf48764
SA001 <smagin.artem@gmail.com>
2020-07-20 22:30:21 +0300

    d

--- CloudStorage/src/main/java/com/example/storage/repository/TariffRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.storage.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.storage.model.Tariff;
-
-public interface TariffRepository extends JpaRepository<Tariff, Integer> {
-
-}
