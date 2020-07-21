522524da174ad5195350992ccf5cfc5e22a0ff2d
DanilovAlexey <danilov_av90@list.ru>
2020-07-21 22:23:00 +0300

    добавил изменения Влада

--- CloudStorage/src/main/java/com/example/storage/controllers/LoginController.java
+++ CloudStorage/src/main/java/com/example/storage/controllers/LoginController.java
@@ -1,7 +1,13 @@
 package com.example.storage.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,13 +27,16 @@ public class LoginController {
 	@Autowired
 	@Qualifier("userServiceImpl")
 	private UserService userService;
-	
+
 	@Autowired
 	@Qualifier("tariffServiceImpl")
 	private TariffService tariffService;
-	
+
 	@Autowired
-	private PasswordEncoder bcryptPasswordEncoder; 
+	private PasswordEncoder bcryptPasswordEncoder;
+
+	@Value("${cloud.folder.root}")
+	private String cloudFolder;
 
 	@GetMapping("/login")
 	public String loginPage(Model model, @RequestParam(name = "error", required = false) Boolean error) {
@@ -39,20 +48,34 @@ public class LoginController {
 	}
 
 	@GetMapping({ "/register" })
-	public String register(Model model) {
+	public String register(Model model, @RequestParam(name = "error", required = false) Boolean error) {
 		model.addAttribute("form", new User());
+
+		if (error != null && error) {
+			model.addAttribute("error", error);
+		}
 		return "views/register";
 	}
 
 	@PostMapping({ "/register" })
-	public String register(@ModelAttribute User user) {
-		user.setUserRole("user");
-		user.setUserPassword(bcryptPasswordEncoder.encode(user.getUserPassword()));
-		
-		user.setTariff(tariffService.getAllTariffs().get(0));
-		
-		userService.addUser(user);
-		return "redirect:/login";
+	public String register(Model model, @ModelAttribute User user) throws IOException {
+
+		if (userService.getUser(user.getUserEmail()) != null) {
+
+			return "redirect:/register?error=true";
+		} else {
+
+			user.setUserRole("user");
+			user.setUserPassword(bcryptPasswordEncoder.encode(user.getUserPassword()));
+			user.setTariff(tariffService.getAllTariffs().get(0));
+
+			userService.addUser(user);
+
+			System.out.println(user.getUserId());
+			var folder = Files.createDirectory(Paths.get(cloudFolder + File.separator + user.getUserId()));
+
+			return "redirect:/login";
+		}
 	}
 
 }
