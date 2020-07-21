c390728abdc430a5b5d80976a1a170f69db4c0ad
Vladislav13 <vladgitar99@gmail.com>
2020-07-21 21:42:23 +0300

    1.Сделал проверку при регистрации/авторизации 2.Добавил удалиение пользователей

--- CloudStorage/src/main/java/com/example/storage/controllers/AdminController.java
+++ CloudStorage/src/main/java/com/example/storage/controllers/AdminController.java
@@ -1,9 +1,13 @@
 package com.example.storage.controllers;
 
-import java.util.Comparator;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +17,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.storage.model.Tariff;
 import com.example.storage.model.User;
 import com.example.storage.service.TariffService;
 import com.example.storage.service.UserService;
@@ -25,49 +27,66 @@ import com.example.storage.service.UserService;
 @Secured("ROLE_admin")
 @RequestMapping("/admin")
 public class AdminController {
-	
+
 	@Autowired
 	@Qualifier("userServiceImpl")
 	private UserService userService;
-	
+
 	@Autowired
 	@Qualifier("tariffServiceImpl")
 	private TariffService tariffService;
 
+	@Value("${cloud.folder.root}")
+	private String cloudFolder;
+
 	@GetMapping("")
 	public ModelAndView admin() {
 		var modelAndView = new ModelAndView();
-		
+
 		var usersList = userService.getAllUsers();
-		usersList.sort((o1, o2) -> o2.getUserId() > o1.getUserId()  ? 1 : -1);
-		
+		usersList.sort((o1, o2) -> o2.getUserId() > o1.getUserId() ? 1 : -1);
+
 		modelAndView.setViewName("views/admin/admin");
 		modelAndView.addObject("users", usersList);
-		
+
 		return modelAndView;
 	}
-	
+
 	@GetMapping("/edit/user/{id}")
-	public ModelAndView editUser(@PathVariable(name="id") Integer id) {
+	public ModelAndView editUser(@PathVariable(name = "id") Integer id) {
 		var modelAndView = new ModelAndView();
-		
+
 		modelAndView.setViewName("views/admin/edit");
 		modelAndView.addObject("user", userService.getUserById(id));
 		modelAndView.addObject("tariffs", tariffService.getAllTariffs());
-		
+
 		return modelAndView;
 	}
-	
-	@PostMapping("/edit") 
-	public String editUser(Model model, 
-			@ModelAttribute User user, 
-			@RequestParam("tariffId") Integer tariffId) {
-	
-		
+
+	@PostMapping("/edit")
+	public String editUser(Model model, @ModelAttribute User user, @RequestParam("tariffId") Integer tariffId) {
+
 		user.setTariff(tariffService.getTariffById(tariffId));
 		userService.updateUser(user);
-		
+
 		return "redirect:/admin";
 	}
-	
+
+	@GetMapping("/delete/user/{id}")
+	public ModelAndView deleteUser(@PathVariable(name = "id") Integer id) {
+		var modelAndView = new ModelAndView();
+		modelAndView.setViewName("views/admin/delete");
+		modelAndView.addObject("user", userService.getUserById(id));
+
+		return modelAndView;
+	}
+
+	@PostMapping("/delete/{id}")
+	public String deleteUserById(@PathVariable(name = "id") Integer id) throws IOException {
+		userService.deleteUser(userService.getUserById(id));
+		Files.delete(Paths.get(cloudFolder + File.separator + id));
+
+		return "redirect:/admin";
+	}
+
 }
